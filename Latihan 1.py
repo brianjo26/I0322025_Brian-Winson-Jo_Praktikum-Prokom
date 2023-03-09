9f004aadc78a574845e34878418178593e45cac0
brianjo26 <brianwinsonjo2654@student.uns.ac.id>
2023-03-09 19:43:29 +0700

    Excercise_01

--- Latihan 1.py
+++ Latihan 1.py
@@ -1,2 +1,10 @@
-print("Brian Winson Jo")
+print("/n"+"Brian Winson Jo")
 print("I0322025")
+
+print("="*20)
+
+print("""
+    Perkenalkan nama saya Brian Winson Jo, NIM I0322025, mahasiswa Teknik Industri UNS angkatan 2022. Saya berasal dari Kota Bogor dengan alamat Komplek Pakuan, Tajur
+    Saya lahir dan besar di Kota Bogor, saat ini saya memilih untuk merantau mencari ilmu di Universitas Sebelas Maret Surakarta. Lingkungan di Kota Bogor tentunya berbeda dengan Kota Surakarta. Menurut saya, suasana di Kota Bogor lebih nyaman karena di sana lebih sejuk dibandingkan dengan Kota Surakarta. Mungkin juga karena saya sudah sejak lahir tinggal di Kota Bogor. Walaupun hampir setiap hari di Bogor turun hujan yang membuatnya dikenal sebagai kota hujan.
+    Harapan saya sebagai lulusan Teknik Industri adalah dapat menerapkan segala ilmu yang diberikan baik dalam kuliah maupun organisasi. Saya berharap kelak menjadi lulusan yang sukses sehingga bisa membanggakan universias dan kedua orang tua saya.
+""")
